2400ff93efa278d6c0b9d7e31978f1a30c3cb69b
Muhammad Azlaan Zubair <azlaanzubair4@gmail.com>
2020-03-28 05:37:00 -0700

    successfully may the infected and not infected countries on live map

--- CoronaMap/js/script.js
+++ CoronaMap/js/script.js
@@ -1,46 +1,75 @@
 // FETCHUNG DATA FROM API
 function updateMap()
 {
-    fetch("./data/data.json")
+    fetch("./data/dataset.json")
     .then(response => response.json())
     .then(rsp => {
-        // console.log(rsp.data)
-        rsp.data.forEach(element => {
-            id = element.id,
-            infected = element.infected,
-            recovered = element.recovered
+        rsp.forEach(data => {
+            // console.log(element.country + " ====> " + element.status)
 
-            console.log(infected)
+            // SETTING DATA TO VARIABLE
+            var id = data.id
+            var country = data.country
+            var status = data.status
+            var coordinates = [data.long, data.lat]
+
+            // CREATING CUSTOM MARKERS
+            customMarkers(country, status, coordinates)
         })
     })
 }
 
-updateMap()
+function customMarkers(country, status, coordinates)
+{
+    // SETTING IMAGE ELEMENT FOR MARKER
+    var el = document.createElement('IMG');
+    el.setAttribute('class', 'marker');
+    el.setAttribute('width', '20');
+    el.setAttribute('height', '20');
+    el.setAttribute('alt', 'Custom Image Markers');
+    el.setAttribute('data-name', country);
+    el.setAttribute('data-status', status);
+    el.setAttribute('data-coordinates', coordinates);
 
+    // RETURNING ELEMENT ON CLICK
+    el.setAttribute('onclick', "popUps(this);");
 
-// LOADING MAP TO SERVE RESULTS
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXpsYWFuNCIsImEiOiJjazhhZGh1dngwZ3FzM2RvMWx0d2h2cDRvIn0.-QPY6ON5kK4IcpSpgZYE1w';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10',
-    zoom: 1,
-    center: [0,40]
-});
+    if (status == "infected") {
+        el.setAttribute('src', './icons/virus.svg');
+    } else {
+        el.setAttribute('src', './icons/no-virus.svg');
+    }
 
-// ADDING MARKER TO THE MAP
-var marker = new mapboxgl.Marker({
-        draggable: true,
-        color: "rgb(255, 0, 0)"
-    })
-    .setLngLat([70, 30]) //([LONGITUDE, LATITUDE])
+    // CREATING MARKER FROM THE ELEMENT AND ADDING TO MAP
+    new mapboxgl.Marker(el)
+    .setLngLat(coordinates)
     .addTo(map);
 
-// GETTING MARKER CO-ORDINATES AFTER DRAGGING
-function onDragEnd() {
-    var lngLat = marker.getLngLat();
-    coordinates.style.display = 'block';
-    coordinates.innerHTML =
-    'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-    }
-        
-    marker.on('dragend', onDragEnd);
+}
+
+// CREATING PUPOPS FOR MARKERS
+function popUps(element)
+{
+    // FETCHING DIV FROM DOM TO SHOW INFORMATION
+    var info_popup_div = document.getElementById('info-popup');
+
+    var country = element.getAttribute('data-name');
+    var status = element.getAttribute('data-status');
+    var coordinates = element.getAttribute('data-coordinates');
+
+    // CONVERTING COORDINATES TO TUPLE
+    coordinates = coordinates.split(',');
+
+    // CREATING INFO POPUP FOR CLICKED MARKER
+    info = `<span>${country}</span><br />`+
+            `<span class="${status == 'infected' ? 'red' : 'green'}">${status == 'infected' ? 'Infected' : 'Not-Infected Yet'}</span>`;
+    info_popup_div.style.display = 'block';
+    info_popup_div.innerHTML = info;
+
+    // HIDING POPUP AFTER DELAY
+    setTimeout(function () {
+        info_popup_div.style.display='none';
+    }, 5000);
+}
+
+updateMap()
