5192377b13ae0e45bfaabb9a8dcd11ce49f36b80
Muhammad Azlaan Zubair <azlaanzubair4@gmail.com>
2020-04-02 06:38:25 -0700

    project deleted

--- CoronaVisualizer/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// INITIALIZING MAP 
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXpsYWFuNCIsImEiOiJjazhhZGh1dngwZ3FzM2RvMWx0d2h2cDRvIn0.-QPY6ON5kK4IcpSpgZYE1w';
-    var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10'
-});
